df8607bab486b102072023e2c3d539b8edb5af40
杨兆雨 <yangzhaoyu@digibird.com.cn>
2022-06-22 15:43:48 +0800

    feat: Translation supports include and exclude

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import type { TransformOptions } from '@babel/core'
 import type { Plugin } from 'vite'
+import { normalizePath } from 'vite'
 import { readFileSync } from 'fs'
+import { join } from 'path'
 import { transformSync, loadOptions, DEFAULT_EXTENSIONS } from '@babel/core'
 
 export interface PluginBabelCompilerOptions {
-    babel: TransformOptions,
+    babel: TransformOptions
     apply: Plugin['apply']
+    exclude: string[]
+    include: string[]
 }
 
 export const queryRE = /\?.*$/s
@@ -35,8 +39,28 @@ const ESBUILD_PLUGIN_NAME = 'esbuild-plugin-babel-compiler'
 
 const transformedRegex = new RegExp(`\\.(${DEFAULT_EXTENSIONS.join('|').replace(/\./g, '')})$`)
 
+function evaluate(
+    moduleList: string[] | RegExp[] | undefined,
+    id: string
+): boolean | undefined {
+    const list = [
+        ...moduleList?.map(item => {
+            if (typeof item === 'string') {
+                return normalizePath(join('node_modules', item, '/'))
+            } if (item instanceof RegExp) {
+                return item
+            }
+            throw new Error('transpileDependencies only accepts an array of string or regular expressions')
+        }) || [],
+        '@vite/client',
+        '@vite/env'
+    ]
+    const rule = list.length ? new RegExp(list.join('|')) : null
+    return rule?.test(normalizePath(id))
+}
+
 function PluginDecorator(rawOptions: PluginBabelCompilerOptions): Plugin {
-    const { apply, babel: options } = rawOptions
+    const { apply, babel: options, exclude = [], include = [] } = rawOptions
     return {
         name: VITE_PLUGIN_NAME,
         apply,
@@ -54,8 +78,10 @@ function PluginDecorator(rawOptions: PluginBabelCompilerOptions): Plugin {
                         },
                         ({ path: rawPath }) => {
                             const path = cleanUrl(rawPath)
-                            const shouldTransform = transformedRegex.test(path)
-                            if (!shouldTransform) return
+                            if (!transformedRegex.test(path)) return
+                            if (!evaluate(include, path)) return
+                            if (evaluate(exclude, path)) return
+
                             const code = readFileSync(path, 'utf-8')
                             const { code: transformedCode } = transform(code, {
                                 ...options,
@@ -72,8 +98,9 @@ function PluginDecorator(rawOptions: PluginBabelCompilerOptions): Plugin {
         },
         transform(code: string, rawId: string) {
             const id = cleanUrl(rawId)
-            const shouldTransform = transformedRegex.test(id)
-            if (!shouldTransform) return
+            if (!transformedRegex.test(id)) return
+            if (!evaluate(include, id)) return
+            if (evaluate(exclude, id)) return
 
             const { code: transformedCode, map } = transform(code, {
                 ...options,
